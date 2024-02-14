2fa28cc2b60ed161d399d4a9ccb062c773a56c31
Jun Aishima <jaishima@bnl.gov>
2024-02-14 11:07:47 -0500

    fix import, annotate to be ignored for linting

--- src/redis_json_dict/__init__.py
+++ src/redis_json_dict/__init__.py
@@ -9,3 +9,5 @@ from __future__ import annotations
 from ._version import version as __version__
 
 __all__ = ["__version__"]
+
+from redis_json_dict.redis_json_dict import RedisJSONDict  # noqa: F401
