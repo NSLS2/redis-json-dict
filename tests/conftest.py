efde368cd66980589d7d9bac209333734e16d53e
Jun Aishima <jaishima@bnl.gov>
2024-02-22 10:35:21 -0500

    use github workflow for redis server
     * remove fixture that uses subprocess
     * remove server fixture from tests
     * tests pass when using local conda env and redis server

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,36 +1,11 @@
 from __future__ import annotations
 
-import contextlib
-import subprocess
-import time as ttime
 import uuid
 
 import pytest
 import redis
 
 from redis_json_dict import RedisJSONDict
-
-
-@contextlib.contextmanager
-def redis_startup():
-    try:
-        ps = subprocess.Popen(
-            [
-                "redis-server",
-            ],
-        )
-        ttime.sleep(1.3)  # make sure the process is started
-        yield ps
-    finally:
-        ps.terminate()
-        redis_client = redis.Redis(host="localhost", port=6379)
-        redis_client.shutdown()
-
-
-@pytest.fixture(scope="session")
-def redis_server():  # noqa: PT004
-    with redis_startup() as redis_fixture:  # noqa: F841
-        yield
 
 
 @pytest.fixture()
