12761647298fdecd60591d9701220aed8f997790
Jun Aishima <jaishima@bnl.gov>
2024-02-22 10:48:55 -0500

    ensure that redis is clean before using

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,6 +11,7 @@ from redis_json_dict import RedisJSONDict
 @pytest.fixture()
 def d():
     redis_client = redis.Redis(host="localhost", port=6379)
+    redis_client.flushall()
     prefix = uuid.uuid4().hex
     yield RedisJSONDict(redis_client, prefix=prefix)
     # Clean up.
